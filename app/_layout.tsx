f2d2ff313aecf61bd754e1b743b9a60f2ec88515
iangelmanuel <iangelmanueldm@gmail.com>
2025-05-10 02:40:45 -0500

    Add configuration files for Babel, Metro, Tailwind CSS, and NativeWind
    - Created babel.config.js to set up Babel with Expo and NativeWind presets.
    - Added metro.config.js to configure Metro bundler with NativeWind support and global CSS input.
    - Introduced nativewind-env.d.ts for TypeScript support with NativeWind types.
    - Established tailwind.config.js to define Tailwind CSS configuration, including custom colors and font families.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,26 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { useFonts } from "expo-font"
+import { Slot, SplashScreen } from "expo-router"
+import { useEffect } from "react"
+import "./global.css"
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  const [loaded, error] = useFonts({
+    "Inter-Black": require("../assets/fonts/WorkSans-Black.ttf"),
+    "Inter-Light": require("../assets/fonts/WorkSans-Light.ttf"),
+    "Inter-Medium": require("../assets/fonts/WorkSans-Medium.ttf")
+  })
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+  useEffect(() => {
+    if (error) throw error
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    if (loaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded, error])
+
+  if (!loaded && !error) return null
+
+  return <Slot />
 }
